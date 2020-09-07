e5d6ac7383e73a371477f797d33f5447e96e9c87
Chris Fein <56209868+Chris-Fein@users.noreply.github.com>
2020-09-07 13:32:12 -0500

    1.20200907: Added jobs, fixed formatting.

--- petparty.lua
+++ petparty.lua
@@ -1,8 +1,9 @@
 _addon.name     = "petparty"
 _addon.author   = "Elidyr"
-_addon.version  = "1.20200906"
+_addon.version  = "1.20200907"
 _addon.command  = "pp"
 
+local res = require("resources")
 local packets = require("packets")
 local texts = require("texts")
 local images = require("images")
@@ -11,8 +12,9 @@ require("texts")
 require("logger")
 
 -- ADJUST YOU BAR COLORS HERE!
-local colors = {owner={r=10,g=250,b=230}, pet={r=10,g=250,b=100}, hpp={r=200,g=100,b=200}, tp={r=200,g=100,b=200}}
+local colors = {owner={r=10,g=250,b=230}, pet={r=10,g=250,b=100}, hpp={r=200,g=100,b=200}}
 local timer = {clock=0, delay=5}
+local jobs = {}
 local pets = {}
 local bar_settings = {
     ["pos"]={["x"]=200,["y"]=200},["bg"]={["alpha"]=255,["red"]=0,["green"]=0,["blue"]=0,["visible"]=false},['flags']={["right"]=false,["bottom"]=false,["bold"]=false,["draggable"]=true,["italic"]=false},["padding"]=5,
@@ -39,7 +41,12 @@ local paths = {
     ["Fenrir"] = ("/icons/fenrir.png"),
     ["Diabolos"] = ("/icons/diabolos.png"),
     ["Siren"] = ("/icons/siren.png"),
-    ["Other"] = ("/icons/rages.png"),
+    [9] = ("/icons/bst.png"),
+    [10] = ("/icons/brd.png"),
+    [14] = ("/icons/drg.png"),
+    [18] = ("/icons/pup.png"),
+    [21] = ("/icons/geo.png"),
+    ["Default"] = ("/icons/default.png"),
 }
 
 local bar = texts.new("", bar_settings)
@@ -67,6 +74,7 @@ windower.register_event("prerender", function()
     
     if (os.clock()-timer.clock) > timer.delay then
         local party = windower.ffxi.get_party() or false
+        local size = (party.party1_count+party.party2_count+party.party3_count)
         local temp = {}
         local count = 0
         local pos = 0
@@ -84,12 +92,11 @@ windower.register_event("prerender", function()
                             owner = string.format("%s,%s,%s", colors.owner.r, colors.owner.g, colors.owner.b),
                             pet = string.format("%s,%s,%s", colors.pet.r, colors.pet.g, colors.pet.b),
                             hpp = string.format("%s,%s,%s", colors.hpp.r, colors.hpp.g, colors.hpp.b),
-                            tp = string.format("%s,%s,%s", colors.tp.r, colors.tp.g, colors.tp.b),
                         }
                         
                         if owner and pet and pet.hpp > 0 then
-                            table.insert(temp, string.format(" \\cs(%s)%+10s\\cr\\cs(%s)[%s]\\cr - \\cs(%s)HPP: %s%%\\cr \\cs(%s)TP: %-10s\\cr", color.owner, owner.name, color.pet, pet.name, color.hpp, pet.hpp or 0, color.tp, pet.tp or 0))
-                        
+                            table.insert(temp, string.format(" \\cs(%s)%+15s\\cr\\cs(%s) [%s]\\cr%s► \\cs(%s)%s%%\\cr", color.owner, (owner.name):sub(1,12), color.pet, (pet.name):sub(1,10), ("----------"):sub(1, (10-(pet.name):len())), color.hpp, pet.hpp or 0))
+                            
                             if not icons[count] then
                                 icons[count] = images.new({color={alpha = 255},texture={fit = false},draggable=false})
                             end
@@ -105,11 +112,22 @@ windower.register_event("prerender", function()
                                 icons[count]:pos_y((bar:pos_y() + (pos*size))+2)
                                 icons[count]:show()
                                 
-                            else
+                            elseif paths[jobs[owner.id]] then
                                 local size = bar_settings.text.size+5
     
                                 -- Update Icon.
-                                icons[count]:path(string.format("%sicons/other.png", windower.addon_path))
+                                icons[count]:path(string.format("%sicons/%s.png", windower.addon_path, (res.jobs[jobs[owner.id]].ens):lower()))
+                                icons[count]:size(size, size)
+                                icons[count]:transparency(0)
+                                icons[count]:pos_x(bar:pos_x())
+                                icons[count]:pos_y((bar:pos_y() + (pos*size))+2)
+                                icons[count]:show()
+                                
+                            else
+                                local size = bar_settings.text.size+5
+                                
+                                -- Update Icon.
+                                icons[count]:path(string.format("%sicons/default.png", windower.addon_path))
                                 icons[count]:size(size, size)
                                 icons[count]:transparency(0)
                                 icons[count]:pos_x(bar:pos_x())
@@ -120,9 +138,9 @@ windower.register_event("prerender", function()
                             pos = (pos + 1)
                             
                         end
-                    
-                    elseif v.mob and v.mob.pet_index == nil then
                         
+                    elseif v.mob and v.mob.pet_index == nil then
+
                         if icons[count] then
                             icons[count]:hide()
                         end
@@ -141,6 +159,31 @@ windower.register_event("prerender", function()
             
         end
         timer.clock = os.clock()
+        
+    end
+    
+end)
+
+windower.register_event("incoming chunk", function(id,original,modified,injected,blocked)
+    
+    if id == 0x0dd then
+        local index = original:unpack("H", 0x18+1)
+        local job = original:unpack("C", 0x22+1)
+        local player = windower.ffxi.get_mob_by_index(index) or false
+        
+        if player then
+            jobs[player.id] = job
+            icons = {}
+        end
+        
+    elseif id == 0x0df then
+        local index = original:unpack("H", 0x14+1)
+        local job = original:unpack("C", 0x20+1)
+        local player = windower.ffxi.get_mob_by_index(index) or false
+        
+        if player and isInParty(player.id) then
+            jobs[player.id] = job
+        end
         
     end
     
